c9835a30c643822ab75c5adcde31a27773941f98
michaelz126 <1zhang.michael@gmail.com>
2025-05-05 00:04:08 +0000

    finished

--- FrogSimulation.java
+++ FrogSimulation.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 public class FrogSimulation {
 
     /** Distance, in inches, from the starting position to the goal. */
@@ -21,7 +22,10 @@ public class FrogSimulation {
      * frog hops.
      */
     private int hopDistance() {
-        /* implementation not shown */ }
+        Scanner s = new Scanner(System.in);
+        System.out.println("How far?");
+        return s.nextInt();
+     }
 
     /**
      * Simulates a frog attempting to reach the goal as described in part (a).
@@ -30,7 +34,14 @@ public class FrogSimulation {
      * false otherwise.
      */
     public boolean simulate() {
-        /* to be implemented in part (a) */ }
+        int distance = 0;
+        for (int i = 0; i < maxHops; i++) {
+            distance += hopDistance();
+            if (distance < 0) return false;
+            if (distance >= goalDistance) return true;
+        }
+        return distance >= goalDistance;
+    }
 
     /**
      * Runs num simulations and returns the proportion of simulations in which the
@@ -38,5 +49,10 @@ public class FrogSimulation {
      * Precondition: num > 0
      */
     public double runSimulations(int num) {
-        /* to be implemented in part (b) */ }
+        double success = 0;
+        for (int i = 0; i < num; i ++) {
+            if (simulate()) success ++;
+        }
+        return success / num ;
+    }
 }
